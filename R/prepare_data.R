d498fa000f386c8d6b59d7211879abf019367f52
Nicole Wang <congwang@tamu.edu>
2023-12-09 16:27:29 -0600

    change comment

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -4,7 +4,7 @@ prepare_data <- function(data, response_var) {
     stop("The response variable is not found in the dataset.")
   }
 
-  # filter out all binary variables
+  # keep all binary variables
   binary_vars <- sapply(data, function(x) all(x %in% c(0, 1)))
   data[, c(response_var, names(binary_vars[binary_vars]))]
 }
